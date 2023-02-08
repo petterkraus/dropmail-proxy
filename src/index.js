5e36d6ba022a47939229eb64089ee8f6eb36d4b9
Petter Kraus <petterkraus@gmail.com>
2023-02-08 13:06:54 -0300

    CORs configured to allow acess

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-
+const cors = require('cors');
 app.use(express.json());
+app.use(cors());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
